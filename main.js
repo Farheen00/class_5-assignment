5d1c7404b81e3f9c30ae5304e99aa1e2e10fb2a3
Farheen00 <159334516+Farheen00@users.noreply.github.com>
2024-03-05 01:17:59 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+//camelCase
+var myFriend = "Maaz";
+//snake_case
+var my_friend = "Ali";
+//PacalCase
+var MyFriend = "Marium";
+console.log(myFriend, my_friend, MyFriend);
+//arrays
+//string type array
+var my_friends = ["Maaz", "Ali", "Marium"];
+console.log(my_friends);
+//to print a particular friend name
+console.log(my_friends[0]);
+console.log(my_friends[1]);
+//to replace any name from the array
+my_friends[2] = "Mehreen";
+console.log(my_friends[2]);
+console.log(my_friends);
+//number type array
+var score = [9, 8, 7, 10];
+console.log(score);
+console.log(score[0], score[1]);
+//boolean type array
+var isPass = [true, false];
+console.log(isPass[0]);
---
+++ main.ts
@@ -0,0 +1,25 @@
+//camelCase
+let myFriend :string = "Maaz"; 
+//snake_case
+let my_friend :string = "Ali";
+//PacalCase
+let MyFriend : string = "Marium";
+console.log(myFriend ,my_friend, MyFriend);
+//arrays
+//string type array
+let my_friends : string[] =["Maaz","Ali" ,"Marium"];
+console.log(my_friends);
+//to print a particular friend name
+console.log(my_friends[0]);
+console.log(my_friends[1]);
+//to replace any name from the array
+my_friends[2] = "Mehreen";
+console.log(my_friends[2]);
+console.log(my_friends);
+//number type array
+let score : number[] =[9,8,7,10];
+console.log(score);
+console.log(score[0],score[1]);
+//boolean type array
+let isPass : boolean[] = [true,false];
+console.log(isPass[0]);
